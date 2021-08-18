c11b094259f97896542e42fcb69fbca3a5c6ab0d
Fredrik Simonsson <simson@polkadex.trade>
2021-08-18 14:16:38 +0900

    Update script

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,3 +11,4 @@ fi
 rustup toolchain add nightly-2021-05-11
 rustup target add wasm32-unknown-unknown --toolchain nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly-2021-05-11
+rustup target add x86_64-unknown-linux-gnu --toolchain nightly-2021-05-11
